b6b1fc78432dea49e157b68f61f97f4ec7f53c90
Gabrielxdf <gabriel.henrique@mail.com>
2023-07-18 17:21:56 -0300

    routes

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,8 +15,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+# HTTP REQUEST <- HTTP RESPONSE
+
+
+# HTTP REQUEST
+def home(request):
+    return HttpResponse("HOME")
+    # return HTTP Response
+
+
+def sobre(request):
+    return HttpResponse("SOBRE")
+
+
+def contato(request):
+    return HttpResponse("CONTATO")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),  # Home
+    path('sobre/', sobre),  # /sobre/
+    path('contato/', contato),  # /contato/
 ]
