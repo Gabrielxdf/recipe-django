70eacfd709c8591a9941659cd9cf096be83748d7
Gabrielxdf <gabriel.henrique@mail.com>
2023-07-21 02:27:56 -0300

    styling the header

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,8 @@
-from django.http import HttpResponse
-
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        'name':'Gabriel Nascimento',
-        'idade':'23',
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Gabriel Nascimento',
+        'idade': '23',
     })
-
-
-def sobre(request):
-    return HttpResponse("SOBRE")
-
-
-def contato(request):
-    return HttpResponse("CONTATO")
