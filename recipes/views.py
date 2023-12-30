468b45f302bbdbb1c55a4218f44484676bc0fcf4
Gabrielxdf <gabriel.henrique@mail.com>
2023-12-30 20:25:21 -0300

    using faker to fullfil the data and some tweaks to the recipe page

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 
+from utils.recipe.factory import make_recipe
+
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Gabriel Nascimento',
-        'idade': '23',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Luiz Otávio',
+        'recipe': make_recipe(),
     })
