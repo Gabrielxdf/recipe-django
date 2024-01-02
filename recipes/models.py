4f5b7b5bb2901d80a793e4b597255f67df9af267
Gabrielxdf <gabriel.henrique@mail.com>
2024-01-02 20:45:27 -0300

    working with Django Admin and media config

--- recipes/models.py
+++ recipes/models.py
@@ -2,9 +2,11 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
@@ -15,13 +17,18 @@ class Recipe(models.Model):
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
-    preparation_step = models.TextField()
-    preparation_step_is_html = models.BooleanField(default=False)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True)
+        Category, on_delete=models.SET_NULL, null=True
+    )
     author = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True)
+        User, on_delete=models.SET_NULL, null=True
+    )
+
+    def __str__(self):
+        return self.title
