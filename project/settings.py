755df636917d5e402ffbe28febf37e3834004335
Gabrielxdf <gabriel.henrique@mail.com>
2024-01-05 23:37:05 -0300

    # noqa: E501 on lines too long

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0y)ly&jp&5^^5u6ww!5t-ai^@tex^&+&%di!9u14qma!x**n5n'
+SECRET_KEY = 'django-insecure-0y)ly&jp&5^^5u6ww!5t-ai^@tex^&+&%di!9u14qma!x**n5n'  # noqa: E501
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,16 +89,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa: E501
     },
 ]
 
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
-from utils.recipe.factory import make_recipe
-
 from .models import Recipe
 
 
