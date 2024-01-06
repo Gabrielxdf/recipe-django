f5b300bed64323c449235b1b664ee5ae5fc4632f
Gabrielxdf <gabriel.henrique@mail.com>
2024-01-06 00:59:40 -0300

    introducing pytest

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        print("HELLO WORLD")
+        print("I'M TESTING")
+        print("THIS WILL NOT PRINT WITHOUT -rP")
+        assert 1 == 1, '1 é igual a 1'
