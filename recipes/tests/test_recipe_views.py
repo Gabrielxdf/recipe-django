d74ca931037e6538d5d621945bec3faee824581d
Gabrielxdf <gabriel.henrique@mail.com>
2024-01-10 00:59:14 -0300

    testing if templates do not load recipes if not published

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -52,7 +52,7 @@ class RecipeViewsTest(RecipeTestBase):
         # content test example, checks if the recipe title is in content
         self.assertIn('Recipe Title', content)
 
-    def test_recipe_home_template_dont_load_recipe_not_published(self):
+    def test_recipe_home_template_dont_load_recipes_not_published(self):
         """Test if recipe is_published False doesnt show"""
         # Need a recipe for this test
         self.make_recipe(is_published=False)
@@ -94,6 +94,18 @@ class RecipeViewsTest(RecipeTestBase):
         # check if one recipe exists
         self.assertIn(needed_title, content)
 
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        """Test if recipe is_published False doesnt show"""
+
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(
+            reverse('recipes:category', kwargs={
+                    'category_id': recipe.category.id})
+        )
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_detail_view_is_correct(self):
         """Testing if the URL calls the correct view function."""
 
@@ -125,3 +137,13 @@ class RecipeViewsTest(RecipeTestBase):
 
         # check if one recipe exists
         self.assertIn(needed_title, content)
+
+    def test_recipe_detail_template_dont_load_recipe_not_published(self):
+        """Test if recipe is_published False doesnt show"""
+
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.id}))
+        self.assertEqual(response.status_code, 404)
