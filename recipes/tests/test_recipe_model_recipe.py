74bf5456eb0999d3c2f7e0b01da51bbe51451b66
Gabrielxdf <gabriel.henrique@mail.com>
2024-01-10 17:31:29 -0300

    creating default parameterized subtests

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -11,4 +11,17 @@ class RecipeModelTest(RecipeTestBase):
     def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
         self.recipe.title = 'A' * 70
         with self.assertRaises(ValidationError):
-            self.recipe.full_clean()  # Here's where the django validation occurs
+            self.recipe.full_clean()  # Here's where the validation occurs
+
+    def test_recipe_fields_max_length(self):
+        fields = [
+            ('title', 65),
+            ('description', 165),
+            ('preparation_time_unit', 65),
+            ('servings_unit', 65),
+        ]
+        for field, max_length in fields:
+            with self.subTest(field=field, max_length=max_length):
+                setattr(self.recipe, field, 'A' * (max_length + 0))
+                with self.assertRaises(ValidationError):
+                    self.recipe.full_clean()
