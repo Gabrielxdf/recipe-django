07db447a8507aad00563e157280eb600c770136c
Gabriel Henrique <gabrielxdf16@gmail.com>
2024-01-14 00:11:31 -0300

    testing if search_term is safely escaped

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -153,7 +153,7 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIs(resolved.func, views.search)
 
     def test_recipe_search_loads_correct_template(self):
-        response = self.client.get(reverse('recipes:search') + '?q=teste')
+        response = self.client.get(reverse('recipes:search') + '?q=test')
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
         """ In this point, for the search feature, we already have a url that
         calls a view, and that view renders a correct template. """
@@ -161,3 +161,12 @@ class RecipeViewsTest(RecipeTestBase):
     def test_recipe_search_raises_404_if_no_search_term(self):
         response = self.client.get(reverse('recipes:search'))
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_search_term_is_on_page_title_and_escaped(self):
+        url = reverse('recipes:search') + '?q=<test>'
+        response = self.client.get(url)
+        self.assertIn(
+            # Search for "<test>", but escaped
+            'Search for &quot;&lt;test&gt;&quot;',
+            response.content.decode('utf-8')
+        )
