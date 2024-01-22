6277628f29ade607a124e1937d7fff3d5e5bdc81
Gabrielxdf <gabriel.henrique@mail.com>
2024-01-22 01:19:39 -0300

    more environment variables with python-dotenv (SECRET_KEY AND DEBUG)

--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0y)ly&jp&5^^5u6ww!5t-ai^@tex^&+&%di!9u14qma!x**n5n'  # noqa: E501
+SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
+# DEBUG = os.environ.get("DEBUG", "0") == "1" <- Without if
 
 ALLOWED_HOSTS = []
 
