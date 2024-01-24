6996bad90e91f64a567def478065cf95da7fd0d3
Gabriel Henrique <gabrielxdf16@gmail.com>
2024-01-24 15:26:16 -0300

    creating a new app for users to register

--- project/settings.py
+++ project/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipes'
+    # our apps
+    'recipes',
+    'authors',
 ]
 
 MIDDLEWARE = [
