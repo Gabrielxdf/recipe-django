4b6c49c6b8ee45304dd727fb1bd6d6170d365f0e
Gabrielxdf <gabriel.henrique@mail.com>
2024-02-23 21:40:42 -0300

    refactoring code

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 from selenium.webdriver.common.by import By
 
@@ -6,7 +8,9 @@ from .base import RecipeBaseFunctionalTest
 
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+    @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_error_not_found_message(self):
+        # self.make_recipe_in_batch(20)
         self.browser.get(self.live_server_url)
         # self.sleep(6)
         body = self.browser.find_element(By.TAG_NAME, 'body')
