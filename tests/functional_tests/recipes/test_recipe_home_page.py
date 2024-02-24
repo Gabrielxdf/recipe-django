728ed68b72b6755e9efa10f25df73f849642cd40
Gabrielxdf <gabriel.henrique@mail.com>
2024-02-24 16:57:22 -0300

    finding inputs and typing texts and keys using selenium

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -2,16 +2,44 @@ from unittest.mock import patch
 
 import pytest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 from .base import RecipeBaseFunctionalTest
 
 
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
-    @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_error_not_found_message(self):
-        # self.make_recipe_in_batch(20)
         self.browser.get(self.live_server_url)
-        # self.sleep(6)
         body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('No recipes found here 😢', body.text)
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_user_recipe_search_input_can_find_correct_recipes(self):
+        recipes = self.make_recipe_in_batch()
+
+        title_needed = 'This is what i need'
+
+        recipes[0].title = title_needed
+        recipes[0].save()
+
+        # User opens the page
+        self.browser.get(self.live_server_url)
+
+        # Sees a search input with the placeholder "Search for a recipe"
+        search_input = self.browser.find_element(
+            By.XPATH,
+            '//input[@placeholder="Search for a recipe"]'
+        )
+
+        # Click in the input and type the search term, then clicks ENTER
+        # to find the recipe with the desired title
+        search_input.send_keys(title_needed)
+        search_input.send_keys(Keys.ENTER)
+
+        # The user sees what was looking for in the page
+        self.assertIn(
+            title_needed,
+            self.browser.find_element(By.CLASS_NAME, 'main-content-list').text,
+        )
+        self.sleep(4)
