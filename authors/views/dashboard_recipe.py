8abd833252e2d03d619ce66a24f71984eb52046d
Gabrielxdf <gabriel.henrique@mail.com>
2024-03-24 23:14:21 -0300

    creating POST method and reorganizing the base View class

--- authors/views/dashboard_recipe.py
+++ authors/views/dashboard_recipe.py
@@ -9,16 +9,37 @@ from recipes.models import Recipe
 
 
 class DashboardRecipe(View):
+    def get_recipe(self, id):
+        recipe = None
+
+        if id:
+            recipe = Recipe.objects.filter(
+                is_published=False,
+                author=self.request.user,
+                pk=id,
+            ).first()
+
+            if not recipe:
+                raise Http404()
+
+        return recipe
+
+    def render_recipe(self, form):
+        return render(
+            self.request,
+            'authors/pages/dashboard_recipe.html',
+            context={
+                'form': form
+            }
+        )
+
     def get(self, request, id):
-        recipe = Recipe.objects.filter(
-            is_published=False,
-            author=request.user,
-            pk=id,
-        ).first()
+        recipe = self.get_recipe(id)
+        form = AuthorRecipeForm(instance=recipe)
+        return self.render_recipe(form)
 
-        if not recipe:
-            raise Http404()
-
+    def post(self, request, id):
+        recipe = self.get_recipe(id)
         form = AuthorRecipeForm(
             data=request.POST or None,
             files=request.FILES or None,
@@ -35,16 +56,9 @@ class DashboardRecipe(View):
 
             recipe.save()
 
-            messages.success(
-                request, 'Your recipe has been saved successfully!')
-            return redirect(reverse(
-                'authors:dashboard_recipe_edit', args=(id,))
+            messages.success(request, 'Sua receita foi salva com sucesso!')
+            return redirect(
+                reverse('authors:dashboard_recipe_edit', args=(id,))
             )
 
-        return render(
-            request,
-            'authors/pages/dashboard_recipe.html',
-            context={
-                'form': form,
-            }
-        )
+        return self.render_recipe(form)
