8ad9ab8bb2170041103e9d1caaa825bd080ab6a8
Gabrielxdf <gabriel.henrique@mail.com>
2024-04-02 17:06:49 -0300

    RecipeListViewSearch

--- recipes/views.py
+++ recipes/views.py
@@ -55,9 +55,45 @@ class RecipeListViewCategory(RecipeListViewBase):
 
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
+
         title = f"{ctx.get('recipes')[0].category.name} - Category | "
+
         ctx.update(
             {'title': title}
+        )
+        return ctx
+
+
+class RecipeListViewSearch(RecipeListViewBase):
+    template_name = 'recipes/pages/search.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        search_term = self.request.GET.get('q', '').strip()
+
+        if not search_term:
+            raise Http404()
+
+        qs = qs.filter(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+            is_published=True,
+        )
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+
+        search_term = self.request.GET.get('q', '').strip()
+        page_title = f'Search for "{search_term}" |'
+        additional_url_query = f'&q={search_term}'
+
+        ctx.update(
+            {
+                'title': page_title,
+                'search_term': search_term,
+                'additional_url_query': additional_url_query
+            }
         )
         return ctx
 
