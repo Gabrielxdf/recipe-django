14f5109aad924d9f0437dc965b3ba9a0fa7017fc
Gabrielxdf <gabriel.henrique@mail.com>
2024-04-17 22:46:17 -0300

    JSONResponse in DetailView

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,10 @@
 import os
 
 from django.db.models import Q
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from django.http.response import Http404
+from django.http.response import HttpResponse as HttpResponse
 from django.views.generic import DetailView, ListView
 
 from utils.pagination import make_pagination
@@ -45,6 +47,7 @@ class RecipeListViewHome(RecipeListViewBase):
 class RecipeListViewHomeApi(RecipeListViewBase):
     template_name = 'recipes/pages/home.html'
 
+    # method that does the render in CBVs
     def render_to_response(self, context, **response_kwargs):
         recipes = self.get_context_data()['recipes']
         recipes_list = recipes.object_list.values()
@@ -134,3 +137,26 @@ class RecipeDetailView(DetailView):
         })
 
         return ctx
+
+
+class RecipeDetailViewApi(RecipeDetailView):
+    def render_to_response(self, context, **response_kwargs):
+        recipe = self.get_context_data()['recipe']
+        recipe_dict = model_to_dict(recipe)
+
+        recipe_dict['created_at'] = str(recipe.created_at)
+        recipe_dict['updated_at'] = str(recipe.created_at)
+
+        if recipe_dict.get('cover'):
+            recipe_dict['cover'] = self.request.build_absolute_uri() + \
+                recipe_dict['cover'].url[1:]  # removing the initial /
+        else:
+            recipe_dict['cover'] = ''
+
+        del recipe_dict['is_published']
+        del recipe_dict['preparation_steps_is_html']
+
+        return JsonResponse(
+            recipe_dict,
+            safe=False
+        )
