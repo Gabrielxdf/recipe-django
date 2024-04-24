4b87f4dbd385dc99800e7a837273a550acc76dc4
Gabrielxdf <gabriel.henrique@mail.com>
2024-04-24 17:04:35 -0300

    avoiding greedy select with values() method

--- recipes/views.py
+++ recipes/views.py
@@ -165,9 +165,8 @@ class RecipeDetailViewApi(RecipeDetailView):
 
 
 def theory(request, *args, **kwargs):
-    recipes = Recipe.objects.filter(
-        id=F('author__id')
-    ).order_by('-id', 'title')[:10]
+    recipes = Recipe.objects\
+        .values('id', 'title', 'author__username')[:10]
 
     context = {
         'recipes': recipes,
