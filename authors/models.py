0856e63f0f1655d8e7b44ae84e55b0c8a400cd51
Gabrielxdf <gabriel.henrique@mail.com>
2024-05-11 14:35:43 -0300

    Creating a User Profile model

--- authors/models.py
+++ authors/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+User = get_user_model()
+
 # Create your models here.
+
+
+class Profile(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default='', blank=True)
+    cover = models.ImageField(
+        upload_to='authors/covers/%Y/%m/%d/', blank=True, default='',)
