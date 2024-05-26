7018f732c811485b085e00d4266c843583a0a990
Gabrielxdf <gabriel.henrique@mail.com>
2024-05-26 17:18:25 -0300

    Renaming fields and using SerializerMethodField

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -5,3 +5,9 @@ class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=65)
     description = serializers.CharField(max_length=165)
+    public = serializers.BooleanField(source='is_published')
+    preparation = serializers.SerializerMethodField()
+
+    def get_preparation(self, recipe):  # get_field_name
+
+        return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
