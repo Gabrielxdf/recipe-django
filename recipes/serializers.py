f24488c3fd05e9d52588939c971236b3e3508f4a
Gabrielxdf <gabriel.henrique@mail.com>
2024-05-27 15:44:29 -0300

    Related fields in django rest framework

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,4 +1,13 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+
+from tag.models import Tag
+
+
+class TagSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=255)
+    slug = serializers.SlugField()
 
 
 class RecipeSerializer(serializers.Serializer):
@@ -6,8 +15,21 @@ class RecipeSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=65)
     description = serializers.CharField(max_length=165)
     public = serializers.BooleanField(source='is_published')
-    preparation = serializers.SerializerMethodField()
+    preparation = serializers.SerializerMethodField(
+        method_name='any_method_name'
+    )
+    category = serializers.StringRelatedField()  # calls the __str__ method
+    author = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(),
+        many=True,
+    )
+    tag_objects = TagSerializer(
+        many=True, source='tags'
+    )
 
-    def get_preparation(self, recipe):  # get_field_name
+    def any_method_name(self, recipe):  # get_field_name or method_name param
 
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
