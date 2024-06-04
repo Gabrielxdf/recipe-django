0d7a4a94f4961aed3f3d0ace3f8107b3707f7999
Gabrielxdf <gabriel.henrique@mail.com>
2024-06-04 18:47:38 -0300

    Mixins, Generic Views and Pagination in DRF

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -10,29 +12,37 @@ from ..models import Recipe
 from ..serializers import RecipeSerializer, TagSerializer
 
 
-class RecipeAPIv2List(APIView):
-    def get(self, request):
-        recipes = Recipe.objects.get_published()[:10]
-        serializer = RecipeSerializer(
-            instance=recipes,
-            many=True,
-            context={'request': request},
-        )
-        return Response(serializer.data)
+class RecipeAPIv2Pagionation(PageNumberPagination):
+    page_size = 2
 
-    def post(self, request):
-        serializer = RecipeSerializer(
-            data=request.data,
-            context={'request': request},
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save(
-            author_id=1, category_id=1, tags=[1, 2],
-        )  # calls the model's save method
-        return Response(
-            serializer.data,
-            status=status.HTTP_201_CREATED
-        )
+
+class RecipeAPIv2List(ListCreateAPIView):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIv2Pagionation
+
+    # def get(self, request):
+    #     recipes = Recipe.objects.get_published()[:10]
+    #     serializer = RecipeSerializer(
+    #         instance=recipes,
+    #         many=True,
+    #         context={'request': request},
+    #     )
+    #     return Response(serializer.data)
+
+    # def post(self, request):
+    #     serializer = RecipeSerializer(
+    #         data=request.data,
+    #         context={'request': request},
+    #     )
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save(
+    #         author_id=1, category_id=1, tags=[1, 2],
+    #     )  # calls the model's save method
+    #     return Response(
+    #         serializer.data,
+    #         status=status.HTTP_201_CREATED
+    #     )
 
 
 class RecipeAPIv2Detail(APIView):
