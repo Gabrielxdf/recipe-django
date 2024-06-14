739c7cf372b5b8480b3befbe43c5bc6863d006c4
Gabrielxdf <gabriel.henrique@mail.com>
2024-06-14 18:54:17 -0300

    Blocking https methods and creating author's serializer with API security understading.

--- authors/views/__init__.py
+++ authors/views/__init__.py
@@ -1,5 +1,6 @@
 # flake8: noqa
 # To django it seems like all the view functions is at the "module" views.py
 from .all import *
+from .api import *
 from .dashboard_recipe import *
 from .profile import *
